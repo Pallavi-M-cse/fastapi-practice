6ba22980de4ebc49952b9a3c2366e9519138092f
Pallavi-M-cse <shilpallavi2004@gmail.com>
2026-02-12 15:31:17 +0530

    Add service layer and fake database

--- backend/app/routes.py
+++ backend/app/routes.py
@@ -1,13 +1,17 @@
 
 from fastapi import APIRouter
 from app.schemas import UserCreate
+from app.services import add_user,get_all_users
 
 router=APIRouter()
 
 @router.post("/users")
 def create_user(user:UserCreate):
+    created_user=add_user(user.dict())
     return{
         "message":"User created",
-        "user":user
+        "user":created_user
     }
-
+@router.get("/users")
+def list_users():
+    return get_all_users()
---
+++ backend/app/services.py
@@ -0,0 +1,8 @@
+from app.database import fake_users_db
+
+def add_user(user_data:dict):
+    fake_users_db.append(user_data)
+    return user_data
+
+def get_all_users():
+    return fake_users_db
